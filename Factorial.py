ac0cb7b075237eafb7f53c508a3ad15b8971630b
vishwanath <vishwanathchenni@gmail.com>
2020-06-27 18:35:36 +0800

    Comments added to code

--- Factorial.py
+++ Factorial.py
@@ -1,16 +1,18 @@
 # Factorial of a number to be calculated
 number = 5
 fact = 1
+#   Check if given number is zero
 if number == 0:
     print("Factorial of ", number, " is ", number)
+#   Check if given number is equal to one
 elif number == 1:
     print("Factorial of ", number, " is ", number)
+#     Calculate factorial of a number
 else:
     for num in range(1, number+1):
         fact = fact * num
     print("Factorial of ", number, " is ", fact)
 
-# Check if number is o
 if fact % 2 == 0:
     print("Even")
 else:
